a04fd060a21c9143251f64d323b1ef1f63d50647
Andres Almiray <aalmiray@yahoo.com>
2013-02-18 15:30:04 +0100

    First release

--- GeoIpresolveGriffonPlugin.groovy
+++ GeoIpresolveGriffonPlugin.groovy
@@ -38,30 +38,84 @@ class GeoIpresolveGriffonPlugin {
     // URL where documentation can be found
     String documentation = ''
     // URL where source can be found
-    String source = ''
+    String source = 'https://github.com/griffon/griffon-geo-ipresolve-plugin'
     // Map of Bnd directives and/or Manifest entries
     // see http://www.aqute.biz/Bnd/Bnd for reference
-    Map manifest = [
+    /*Map manifest = [
         'Bundle-Description': 'Plugin summary/headline'
-    ]
+    ]*/
 
     List authors = [
         [
-            name: 'Your Name',
-            email: 'your@email.com'
+            name: 'Andres Almiray',
+            email: 'aalmiray@yahoo.com'
         ]
     ]
-    String title = 'Plugin summary/headline'
+    String title = 'GeoIP (MaxMind) Integration'
     // accepts Markdown syntax. See http://daringfireball.net/projects/markdown/ for details
     String description = '''
-Brief description of GeoIpresolve.
+The purpose of this very simple plugin is to provide a seemless integration with
+MaxMind's geographical location open source API and has been built to be used with
+the GeoCity binary database. This is a port of the [geoip][1] Grails plugin
+originally by Radu Andrei Tanasa.
 
 Usage
-----
-Lorem ipsum
+-----
 
-Configuration
--------------
-Lorem ipsum
+You must configure 2 properties in `Config.groovy` using the [Service configuration DSL][2]:
+
+ * `dataPath`. Path to your Maxmind database file. You can get a free Lite version [here][3].
+   The value should point to a classpath resource, for example `GeoLiteCity.dat`;
+   this resource can be placed inside `griffon-app/resources`.
+ * `cacheOption`. There are 4 possible values for this:
+ 
+    * 0 - GEOIP_STANDARD (this is the default)
+    * 1 - GEOIP_MEMORY_CACHE
+    * 2 - GEOIP_CHECK_CACHE
+    * 4 - GEOIP_INDEX_CACHE
+
+Here's an example configuration
+
+    services {
+        geoIpResolve {
+            dataPath = 'GeoLiteCity.dat'
+        }
+    }
+
+Note that when you're using the cached versions it will load the database in memory.
+Its size is usually around 45 MB. 
+
+At the moment the service exposes only one method. Here is how you determine the
+location of an IP:
+
+    class MyController {
+        def geoIpResolveService
+        def doSomething = {
+            def location = geoIpService.getLocation('88.158.9.150')
+            println location
+        }
+    }
+
+Here is a list of the location's fields.
+
+ * countryCode
+ * countryName
+ * region
+ * regionName
+ * city
+ * postalCode
+ * timezone
+ * latitude
+ * longitude
+ * dmaCode
+ * areaCode
+ * metroCode
+
+Note that some may not be properly populated if the information is missing.
+
+
+[1]: http://grails.org/plugin/geoip
+[2]: http://griffon.codehaus.org/guide/latest/guide/controllersAndServices.html#serviceConfiguration
+[3]: http://www.maxmind.com/app/geolitecity
 '''
 }
